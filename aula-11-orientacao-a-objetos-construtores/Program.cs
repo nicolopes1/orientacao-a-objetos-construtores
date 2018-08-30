bb3b445639b0ef7dfc1e1288d77d4187ca18b0f3
nicollas <nicollaslopesjf@gmail.com>
2018-08-30 20:57:44 -0300

    file

---
+++ aula-11-orientacao-a-objetos-construtores/Program.cs
@@ -0,0 +1,37 @@
+﻿using Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula_11_orientacao_a_objetos_construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // atributos de instância
+            var cliente = new Cliente(); 
+            cliente.Nome = "Testeeee";
+            cliente.Telefone = "00000";
+            cliente.CPF = "2323232332323232";
+            cliente.Gravar();
+
+
+            //Cliente c = new Cliente("Nicoooo","3432434432324432","43432432432432");
+
+
+            var clientes = Cliente.LerClientes();
+            foreach (Cliente c in clientes)
+            {
+                Console.WriteLine(c.Nome);
+                Console.WriteLine(c.Telefone);
+                Console.WriteLine(c.CPF);
+            }
+            Console.Read();
+        }
+    }
+}
